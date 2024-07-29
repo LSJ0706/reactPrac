9a07ed93dd64373d3f7d0a8c3e487d998f04167e
LSJ0706 <zosksldhs@gmail.com>
2024-07-29 18:21:15 +0900

    todo

---
+++ todo/src/App.jsx
@@ -0,0 +1,13 @@
+import "./assets/css/global.css";
+
+function App() {
+  return (
+    <>
+      <div id="wrapper">
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ todo/src/modal.jsx
@@ -0,0 +1,7 @@
+import "./assets/css/global.css";
+
+const modal = () => {
+  return <div></div>;
+};
+
+export default modal;
